753804170c92ae14bb44004a1ab93ac61822b01a
natagr23 <nata_garcia23@hotmail.com>
2021-10-06 13:58:32 +0200

    calling buttons

--- scripts/game_cards.js
+++ scripts/game_cards.js
@@ -1,12 +1,98 @@
-const sgv_container = document.getElementById('sgv1')
-console.log(sgv_container);
+// const sgv_container = document.getElementById('sgv1')
+// console.log(sgv_container);
 
-const td_1 = document.getElementById('td1')
-console.log(td_1);
+// const td_1 = document.getElementById('td1')
+// console.log(td_1);
 
-td_1.addEventListener('click', () => {
-    sgv_container.removeAttribute('hidden')
-    });
+// td_1.addEventListener('click', () => {
+
+//     if (sgv_container.classList.contains('hidden')) {
+//         sgv_container.classList.remove('hidden')
+//     }
+//     else {
+//         sgv_container.classList.add('hidden')
+//     }
+
+// });
+const sgv1_container = document.getElementById('sgv1');
+
+const sgv2_container = document.getElementById('sgv2');
+const sgv3_container = document.getElementById('sgv3');
+const sgv4_container = document.getElementById('sgv4');
+const sgv5_container = document.getElementById('sgv5');
+const sgv6_container = document.getElementById('sgv6');
+const sgv7_container = document.getElementById('sgv7');
+const sgv8_container = document.getElementById('sgv8');
+const sgv9_container = document.getElementById('sgv9');
+const sgv10_container = document.getElementById('sgv10');
+const sgv11_container = document.getElementById('sgv11');
+const sgv12_container = document.getElementById('sgv12');
+const sgv13_container = document.getElementById('sgv13');
+const sgv14_container = document.getElementById('sgv14');
+const sgv15_container = document.getElementById('sgv15');
+const sgv16_container = document.getElementById('sgv16');
+
+console.log(sgv1_container);
+console.log(sgv2_container);
+
+const td_all = document.querySelectorAll('td');
+console.log(td_all);
+
+console.log(td_all[1]);
+
+
+td_all[1].addEventListener('click', () => {
+    if (sgv1_container.classList.contains('hidden')
+    ) {
+        sgv1_container.classList.remove('hidden')
+    }
+    else {
+        sgv1_container.classList.add('hidden')
+    }
+});
+
+
+td_all[2].addEventListener('click', () => {
+    if (sgv2_container.classList.contains('hidden')
+    ) {
+        sgv2_container.classList.remove('hidden')
+    }
+    else {
+        sgv2_container.classList.add('hidden')
+    }
+});
+
+
+
+
+// var rowIndex = 0;
+// var cellIndex = 0;
+// let td_container= document.querySelectorAll('td');
+// console.log (td_container);
+
+// let cell_td_container = document.getElementById('cellIndex');
+// console.log (cell_td_container);
+
+// const sgv1_container = document.getElementById('sgv1')
+// console.log(sgv1_container);
+
+// let td_all = document.querySelectorAll('td')
+// // console.log(td_all);
+// console.log(td_all.cell);
+
+
+// td_all[1].addEventListener('click', () => {
+
+//     sgv1_container.removeAttribute('hidden')
+
+//     });
+
+
+
+
+// td_1.addEventListener('click', () => {
+//     sgv_container.removeAttribute('hidden')
+//     });
 
 
 
