7d52dd4eb4f002911b1ce392c24c18b4898855d8
natagr23 <nata_garcia23@hotmail.com>
2021-10-23 05:20:11 -0500

    simplifying the code

--- scripts/game_cards.js
+++ scripts/game_cards.js
@@ -1,312 +1,44 @@
-// const sgv_container = document.getElementById('sgv1')
-// console.log(sgv_container);
+// const div_all = document.querySelectorAll('div');
+// console.log(div_all);
 
-// const td_1 = document.getElementById('td1')
-// console.log(td_1);
+// const td_all = document.getElementsByTagName('td');
+// console.log(td_all);
 
-// td_1.addEventListener('click', () => {
+// const id_td_container = td_all.getElementsByTagName('id')
+// console.log(id_td_container);
 
-//     if (sgv_container.classList.contains('hidden')) {
-//         sgv_container.classList.remove('hidden')
-//     }
-//     else {
-//         sgv_container.classList.add('hidden')
-//     }
 
-// });
-const sgv1_container = document.getElementById('sgv1');
-const sgv2_container = document.getElementById('sgv2');
-const sgv3_container = document.getElementById('sgv3');
-const sgv4_container = document.getElementById('sgv4');
-const sgv5_container = document.getElementById('sgv5');
-const sgv6_container = document.getElementById('sgv6');
-const sgv7_container = document.getElementById('sgv7');
-const sgv8_container = document.getElementById('sgv8');
-const sgv9_container = document.getElementById('sgv9');
-const sgv10_container = document.getElementById('sgv10');
-const sgv11_container = document.getElementById('sgv11');
-const sgv12_container = document.getElementById('sgv12');
-const sgv13_container = document.getElementById('sgv13');
-const sgv14_container = document.getElementById('sgv14');
-const sgv15_container = document.getElementById('sgv15');
-const sgv16_container = document.getElementById('sgv16');
+const div_all = document.querySelectorAll('div');
+console.log(div_all);
 
-console.log(sgv1_container);
-console.log(sgv2_container);
+console.log(div_all[3].firstElementChild);
 
 const td_all = document.querySelectorAll('td');
 console.log(td_all);
 
+// const id_td_container = td_all.querySelector('id')
+// console.log(id_td_container)
+
 console.log(td_all[1]);
-console.log(td_all[2]);
-console.log(td_all[3]);
-console.log(td_all[4]);
-console.log(td_all[5]);
-console.log(td_all[6]);
-console.log(td_all[7]);
-console.log(td_all[8]);
-console.log(td_all[9]);
-console.log(td_all[10]);
-console.log(td_all[11]);
-console.log(td_all[12]);
-console.log(td_all[13]);
-console.log(td_all[14]);
-console.log(td_all[15]);
-console.log(td_all[16]);
 
 
- td_all[1].addEventListener('click', () => {
-    if (sgv1_container.classList.contains('hidden')
-    ) {
-        sgv1_container.classList.remove('hidden');
+for (let i = 0; i < 17; i++ ) {
+    // if (i===5){
+    //     continue;
+    //     }
+        
+            td_all[i].addEventListener('click', () => {
+                
+            if (div_all[i+3].firstElementChild.classList.contains('hidden')
+            ) {
+                div_all[i+3].firstElementChild.classList.remove('hidden');
+            }
+            else {
+                div_all[i+3].firstElementChild.classList.add('hidden');
+            }
+        }
+        )
     }
-    else {
-        sgv1_container.classList.add('hidden');
-    }
-    // if (localStorage.getItem('current_icon_id')=== td_all[1].id) {
-    //     console.log('already clicked')
-    //     td_all[1].remove();
-    //     td_all[11].remove();
-    // } else{
-    //     localStorage.setItem('current_icon_id', td_all[1].id);
-    //     console.log('setting current icon');
-    // }
-});
+    //const new_p = copy_div.querySelector('p');
 
-
-td_all[2].addEventListener('click', () => {
-    if (sgv2_container.classList.contains('hidden')
-    ) {
-        sgv2_container.classList.remove('hidden')
-    }
-    else {
-        sgv2_container.classList.add('hidden')
-    }
-    // if (localStorage.getItem('current_icon_id')=== td_all[2].id) {
-    //     console.log('already clicked')
-    //     td_all[2].remove();
-    //     td_all[12].remove();
-    // } else{
-    //     localStorage.setItem('current_icon_id', td_all[2].id);
-    //     console.log('setting current icon')
-    // }
-});
-
-td_all[3].addEventListener('click', () => {
-    if (sgv3_container.classList.contains('hidden')
-    ) {
-        sgv3_container.classList.remove('hidden')
-    }
-    else {
-        sgv3_container.classList.add('hidden')
-    }
-});
-
-td_all[4].addEventListener('click', () => {
-    if (sgv4_container.classList.contains('hidden')
-    ) {
-        sgv4_container.classList.remove('hidden')
-    }
-    else {
-        sgv4_container.classList.add('hidden')
-    }
-});
-
-td_all[6].addEventListener('click', () => {
-    if (sgv5_container.classList.contains('hidden')
-    ) {
-        sgv5_container.classList.remove('hidden')
-    }
-    else {
-        sgv5_container.classList.add('hidden')
-    }
-});
-
-td_all[7].addEventListener('click', () => {
-    if (sgv6_container.classList.contains('hidden')
-    ) {
-        sgv6_container.classList.remove('hidden')
-    }
-    else {
-        sgv6_container.classList.add('hidden')
-    }
-});
-
-td_all[8].addEventListener('click', () => {
-    if (sgv7_container.classList.contains('hidden')
-    ) {
-        sgv7_container.classList.remove('hidden')
-    }
-    else {
-        sgv7_container.classList.add('hidden')
-    }
-    
-});
-
-td_all[9].addEventListener('click', () => {
-    if (sgv8_container.classList.contains('hidden')
-    ) {
-        sgv8_container.classList.remove('hidden')
-    }
-    else {
-        sgv8_container.classList.add('hidden')
-    }
-    // if (localStorage.getItem('current_icon_id')=== td_all[9].id) {
-    //     console.log('already clicked')
-    //     td_all[19].remove();
-    //     td_all[9].remove();
-    // } else{
-    //     localStorage.setItem('current_icon_id', td_all[9].id);
-    //     console.log('setting current icon')
-    // }
-   
-});
-
-td_all[11].addEventListener('click', () => {
-    if (sgv9_container.classList.contains('hidden')
-    ) {
-        sgv9_container.classList.remove('hidden')
-    }
-    else {
-        sgv9_container.classList.add('hidden')
-    }
-    // if (localStorage.getItem('current_icon_id')=== td_all[11].id) {
-    //     console.log('already clicked')
-    //     td_all[1].remove();
-    //     td_all[11].remove();
-    // } else{
-    //     localStorage.setItem('current_icon_id', td_all[11].id);
-    //     console.log('setting current icon')
-    // }
-});
-
-td_all[12].addEventListener('click', () => {
-    if (sgv10_container.classList.contains('hidden')
-    ) {
-        sgv10_container.classList.remove('hidden')
-    }
-    else {
-        sgv10_container.classList.add('hidden')
-    }
-    // if (localStorage.getItem('current_icon_id')=== td_all[12].id) {
-    //     console.log('already clicked')
-    //     td_all[2].remove();
-    //     td_all[12].remove();
-    // } else{
-    //     localStorage.setItem('current_icon_id', td_all[12].id);
-    //     console.log('setting current icon')
-    // }
-});
-
-td_all[13].addEventListener('click', () => {
-    if (sgv11_container.classList.contains('hidden')
-    ) {
-        sgv11_container.classList.remove('hidden')
-    }
-    else {
-        sgv11_container.classList.add('hidden')
-    }
-});
-
-td_all[14].addEventListener('click', () => {
-    if (sgv12_container.classList.contains('hidden')
-    ) {
-        sgv12_container.classList.remove('hidden')
-    }
-    else {
-        sgv12_container.classList.add('hidden')
-    }
-});
-
-td_all[16].addEventListener('click', () => {
-    if (sgv13_container.classList.contains('hidden')
-    ) {
-        sgv13_container.classList.remove('hidden')
-    }
-    else {
-        sgv13_container.classList.add('hidden')
-    }
-});
-
-td_all[17].addEventListener('click', () => {
-    if (sgv14_container.classList.contains('hidden')
-    ) {
-        sgv14_container.classList.remove('hidden')
-    }
-    else {
-        sgv14_container.classList.add('hidden')
-    }
-});
-
-td_all[18].addEventListener('click', () => {
-    if (sgv15_container.classList.contains('hidden')
-    ) {
-        sgv15_container.classList.remove('hidden')
-    }
-    else {
-        sgv15_container.classList.add('hidden')
-    }
-});
-
-button_td_19 = td_all[19].addEventListener('click', () => {
-    if (sgv16_container.classList.contains('hidden')
-    ) {
-        sgv16_container.classList.remove('hidden')
-    }
-    else {
-        sgv16_container.classList.add('hidden')
-    }
-    
-    // if (localStorage.getItem('current_icon_id')=== td_all[19].id) {
-    //     console.log('already clicked')
-    //     td_all[19].remove();
-    //     td_all[9].remove();
-    // } else{
-    //     localStorage.setItem('current_icon_id', td_all[19].id);
-    //     console.log('setting current icon')
-    // }
-});
-
-// const remove_button_sgv19 = td_all[19].addEventListener('click',() => {
-//     td_all[19].remove()
-// })
-
-
-// let equal_variables = ()=>{ (td_all[19].id === td_all[9].id)};
-
-
-
-
-
-// var rowIndex = 0;
-// var cellIndex = 0;
-// let td_container= document.querySelectorAll('td');
-// console.log (td_container);
-
-// let cell_td_container = document.getElementById('cellIndex');
-// console.log (cell_td_container);
-
-// const sgv1_container = document.getElementById('sgv1')
-// console.log(sgv1_container);
-
-// let td_all = document.querySelectorAll('td')
-// // console.log(td_all);
-// console.log(td_all.cell);
-
-
-// td_all[1].addEventListener('click', () => {
-
-//     sgv1_container.removeAttribute('hidden')
-
-//     });
-
-
-
-
-// td_1.addEventListener('click', () => {
-//     sgv_container.removeAttribute('hidden')
-//     });
-
-
-
-
+    // const div_container = document.getElementById('div_container');
